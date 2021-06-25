e8083041da7fe4582922968db23b845da2b7487f
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-06-25 05:21:43 -0700

    added logic to choose recipe based on user input

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,22 +1,4 @@
-const requestIngredient = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast'
-const requestCatagories = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list'
-const requestArea = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list'
-
-function getApi(requestUrl) {
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();            
-        })
-        .then(function (data) {
-            console.log(data);
-        });
-};
-
-// getApi(requestArea);
-// getApi(requestCatagories);
-// getApi(requestIngredient);
-
-//FOOD RECIPE RESULTS
+//FOOD RECIPE RESULT LOGIC
 const foodBox = document.querySelector("#foodBox");
 
 function getFood(requestURL) {
@@ -26,22 +8,25 @@ function getFood(requestURL) {
         })
         .then(function (data) {
             console.log(data);
+            // Creates img and anchor elements
             const link = document.createElement('a')
             const img = document.createElement('img')
             const foodID = data.meals[0].idMeal
             const foodResult = 'https://www.themealdb.com/meal/' + foodID
             const foodImg = data.meals[0].strMealThumb
             
+            //Sets img to thumbnail from API fetch
             img.src = foodImg;
-
+            //Sets link href and text
             link.textContent = data.meals[0].strMeal;
             link.href = foodResult;
 
+            //Appends img to link text
+            //Appends link text to html div
             link.appendChild(img);
             foodBox.appendChild(link);
         });
 }
-
 
 //RANDOMZIED DRINK CHOICE LOGIC
 const requestRandDrink = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
@@ -55,22 +40,91 @@ function getDrink(requestURL) {
         })
         .then(function (data) {
             console.log(data);
+            // Creates img and anchor elements
             const link = document.createElement('a')
             const img = document.createElement('img')
             const drinkID = data.drinks[0].idDrink
             const drinkResult = 'https://www.thecocktaildb.com/drink/' + drinkID
             const drinkImg = data.drinks[0].strDrinkThumb
 
+            //Sets img to thumbnail from API fetch
             img.src = drinkImg
-            
+            //Sets link href and text            
             link.textContent = data.drinks[0].strDrink
             link.href = drinkResult;
-
-
+            
+            //Appends img to link text
+            //Appends link text to html div
             link.appendChild(img);
             drinkBox.appendChild(link);
         });
 }
 
 getDrink(requestRandDrink);
-getFood(app1);
+
+//USER INPUT -> RECIPE RESULT LOGIC
+userValue = "5"
+const errorBox = document.querySelector("#errorBox");
+
+if (userValue === "1") {
+    getFood(app1);
+} else if (userValue === "2") {
+    getFood(app2);    
+} else if (userValue === "") {
+    getFood(app3);    
+} else if (userValue === "") {
+    getFood(app4);    
+} else if (userValue === "") {
+    getFood(app5);    
+} else if (userValue === "") {
+    getFood(app6);    
+} else if (userValue === "") {
+    getFood(app7);    
+} else if (userValue === "") {
+    getFood(app8);    
+} else if (userValue === "") {
+    getFood(app9);    
+} else if (userValue === "") {
+    getFood(ent1);    
+} else if (userValue === "") {
+    getFood(ent2);    
+} else if (userValue === "") {
+    getFood(ent3);    
+} else if (userValue === "") {
+    getFood(ent4);    
+} else if (userValue === "") {
+    getFood(ent5);    
+} else if (userValue === "") {
+    getFood(ent6);    
+} else if (userValue === "") {
+    getFood(ent7);    
+} else if (userValue === "") {
+    getFood(ent8);    
+} else if (userValue === "") {
+    getFood(ent9);    
+} else if (userValue === "") {
+    getFood(des1);    
+} else if (userValue === "") {
+    getFood(des2);    
+} else if (userValue === "") {
+    getFood(des3);    
+} else if (userValue === "") {
+    getFood(des4);    
+} else if (userValue === "") {
+    getFood(des5);    
+} else if (userValue === "") {
+    getFood(des6);    
+} else if (userValue === "") {
+    getFood(des7);    
+} else if (userValue === "") {
+    getFood(des8);    
+} else if (userValue === "") {
+    getFood(des9);    
+} else {
+    //Creates p element
+    const p = document.createElement('p')
+    //Sets p text
+    p.textContent = "Option unavailable, please try again"
+    //Appends p to html div
+    foodBox.appendChild(p);
+};
